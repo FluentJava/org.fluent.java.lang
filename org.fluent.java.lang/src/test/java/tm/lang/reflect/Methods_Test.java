3341d96676832e71d647fd937a4a1a64c6c7f4a3
DinisCruz-Dev <dinis.cruz+dev@owasp.org>
2014-05-17 10:42:44 +0100

    Added simple unit test to Methods class

--- org.fluent.java.lang/src/test/java/tm/lang/reflect/Methods_Test.java
+++ org.fluent.java.lang/src/test/java/tm/lang/reflect/Methods_Test.java
@@ -1,5 +1,9 @@
 package tm.lang.reflect;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 import tm.lang.Reflection;
 import tm.lang.TestObject;
 
@@ -16,5 +20,11 @@ public class Methods_Test
 		methods	   = reflection.methods; 
 	}
 	
-	
+	@Test
+	public void Methods_Ctor()
+	{
+		assertNotNull(reflection);
+		assertEquals (reflection.target, testObject);
+		assertEquals (reflection, methods.reflection);
+	}
 }
