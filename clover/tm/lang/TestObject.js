1570c4d8a7e9ce45be328577902a25ddd408fbac
DinisCruz-Dev <dinis.cruz+dev@owasp.org>
2014-05-17 04:39:34 +0100

    Publishing Site Docs for 							org.fluent.java.lang:0.9.1-SNAPSHOT

--- clover/tm/lang/TestObject.js
+++ clover/tm/lang/TestObject.js
@@ -4,7 +4,7 @@ var clover = new Object();
 clover.pageData = {"classes":[{"el":26,"id":89,"methods":[{"el":11,"sc":2,"sl":8},{"el":16,"sc":2,"sl":13},{"el":20,"sc":2,"sl":17},{"el":25,"sc":2,"sl":22}],"name":"TestObject","sl":3}]}
 
 // JSON: {test_ID : {"methods": [ID1, ID2, ID3...], "name" : "testXXX() void"}, ...};
-clover.testTargets = {"test_14":{"methods":[{"sl":8},{"sl":13},{"sl":17},{"sl":22}],"name":"invoke","pass":true,"statements":[{"sl":10},{"sl":15},{"sl":19},{"sl":24}]},"test_18":{"methods":[{"sl":8},{"sl":13},{"sl":17},{"sl":22}],"name":"invoke","pass":true,"statements":[{"sl":10},{"sl":15},{"sl":19},{"sl":24}]},"test_2":{"methods":[{"sl":8},{"sl":13},{"sl":17},{"sl":22}],"name":"invoke","pass":true,"statements":[{"sl":10},{"sl":15},{"sl":19},{"sl":24}]}}
+clover.testTargets = {"test_1":{"methods":[{"sl":8},{"sl":13},{"sl":17},{"sl":22}],"name":"invoke","pass":true,"statements":[{"sl":10},{"sl":15},{"sl":19},{"sl":24}]},"test_15":{"methods":[{"sl":8},{"sl":13},{"sl":17},{"sl":22}],"name":"invoke","pass":true,"statements":[{"sl":10},{"sl":15},{"sl":19},{"sl":24}]},"test_18":{"methods":[{"sl":8},{"sl":13},{"sl":17},{"sl":22}],"name":"invoke","pass":true,"statements":[{"sl":10},{"sl":15},{"sl":19},{"sl":24}]},"test_8":{"methods":[{"sl":8},{"sl":13},{"sl":17},{"sl":22}],"name":"invoke","pass":true,"statements":[{"sl":10},{"sl":15},{"sl":19},{"sl":24}]}}
 
 // JSON: { lines : [{tests : [testid1, testid2, testid3, ...]}, ...]};
-clover.srcFileLines = [[], [], [], [], [], [], [], [], [18, 2, 14], [], [18, 2, 14], [], [], [18, 2, 14], [], [18, 2, 14], [], [18, 2, 14], [], [18, 2, 14], [], [], [18, 2, 14], [], [18, 2, 14], [], []]
+clover.srcFileLines = [[], [], [], [], [], [], [], [], [18, 1, 8, 15], [], [18, 1, 8, 15], [], [], [18, 1, 8, 15], [], [18, 1, 8, 15], [], [18, 1, 8, 15], [], [18, 1, 8, 15], [], [], [18, 1, 8, 15], [], [18, 1, 8, 15], [], []]
